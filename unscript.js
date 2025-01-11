69e36740f44bb94a2fbf9a0cab6a68d2e22874f0
encrypted-cookies <105543893+encrypted-cookies@users.noreply.github.com>
2025-01-11 20:52:39 +0100

    Delete unscript.js

--- unscript.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggle-balance').addEventListener('click', function() {
-    var balance = document.querySelector('.balance');
-    var amount = document.querySelector('.amount');
-    
-    balance.classList.toggle('show');
-    amount.classList.toggle('blur');
-  });
